d955901523a77d9770d31f60786389cfc4a2ffcf
microchip123 <lex13post@gmail.com>
2022-10-13 20:27:10 +0300

    Plugin Init

--- Source/MapStepTest/Private/TestCPP.cpp
+++ Source/MapStepTest/Private/TestCPP.cpp
@@ -1,10 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Test.h"
+#include "TestCPP.h"
+
+#include "GameFramework/GameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+
 
 // Sets default values
-ATest::ATest()
+ATestCPP::ATestCPP()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,14 +16,13 @@ ATest::ATest()
 }
 
 // Called when the game starts or when spawned
-void ATest::BeginPlay()
+void ATestCPP::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
-void ATest::Tick(float DeltaTime)
+void ATestCPP::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
--- Source/MapStepTest/Public/TestCPP.h
+++ Source/MapStepTest/Public/TestCPP.h
@@ -4,16 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Test.generated.h"
+#include "TestCPP.generated.h"
 
 UCLASS()
-class MAPSTEPTEST_API ATest : public AActor
+class MAPSTEPTEST_API ATestCPP : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	ATest();
+	ATestCPP();
 
 protected:
 	// Called when the game starts or when spawned
